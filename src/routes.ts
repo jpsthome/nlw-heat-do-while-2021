05623e1e844b9d5fd70b7305f47399558acd1118
Joao Pedro <joao.thome@unila.edu.br>
2021-10-19 18:14:47 -0300

    adicionando busca pelas  3 últimas mensagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticaUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
 router.post(
 	"/messages",
 	ensureAuthenticated,
 	new CreateMessageController().handle,
 );
+
+router.get("/messages/last3", new GetLast3MessagesController().handle);
 
 export { router };
